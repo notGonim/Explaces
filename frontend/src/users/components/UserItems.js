f6b239a037b172fcebebb13ea96a1fabc4a1e1ee
notGonim <mhmodgonim96@gmail.com>
2020-12-20 14:11:20 +0200

    Added card component

--- frontend/src/users/components/UserItems.js
+++ frontend/src/users/components/UserItems.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { Avatar } from '../../shared/components/UIElements/Avatar'
+import { Card } from '../../shared/components/UIElements/Card'
 
 export const UserItems = (props) => {
     return (
         <UserItem>
-            <UserItemContent>
-                <UserItemImage>
-                    <img src={props.image} alt={props.name} />
-                </UserItemImage>
-                <UserItemInfo>
-                    <h2>{props.name}</h2>
-                    <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
-                </UserItemInfo>
-            </UserItemContent>
+            <Card >
+                <Link to={`/${props.id}/places`} >
+                    <UserItemImage>
+                        <Avatar image={props.image} />
+                    </UserItemImage>
+                    <UserItemInfo>
+                        <h2>{props.name}</h2>
+                        <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+                    </UserItemInfo>
+                </Link>
+            </Card>
         </UserItem>
     )
 }
@@ -20,16 +25,46 @@ export const UserItems = (props) => {
 
 
 const UserItem = styled.li`
-
-`
-const UserItemContent = styled.div`
-
+ margin: 1rem;
+  width: calc(45% - 2rem);
+  min-width: 17.5rem;
+  a{
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  text-decoration: none;
+  padding: 1rem;
+  color: white;
+  background: #292929;
+  }
+  a:hover,
+  a:active {
+  background: #0084ff;
+}
+    h2:hover,
+    h2:active,
+    h3:hover,
+    h3:active {
+    color: #292929;
+    }
 `
 
 const UserItemImage = styled.div`
-
+ width: 4rem;
+  height: 4rem;
+  margin-right: 1rem;
 `
 
 const UserItemInfo = styled.div`
+ h2 {
+  font-size: 1.5rem;
+  margin: 0 0 0.5rem 0;
+  font-weight: normal;
+  color: #ffd900;
+}
+h3 {
+  margin: 0;
+}
 
 `
--- frontend/src/users/components/UserList.js
+++ frontend/src/users/components/UserList.js
@@ -34,6 +34,13 @@ const Center = styled.div`
 `
 
 const UsersList = styled.ul`
-
+list-style: none;
+  margin: 0 auto;
+  padding: 0;
+  width: 90%;
+  max-width: 50rem;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 
 `
